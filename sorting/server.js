9dd002d111349e2d8ffa985c7bee3f2cae167e4f
gceresia <Gino@ginos-mbp-2.mobile.asu.edu>
2015-12-08 20:06:43 -0700

    Final

--- sorting/server.js
+++ sorting/server.js
@@ -7,6 +7,10 @@ for (var i = 0; i < jsonObj.length - 1; i++)
   jsonObj[i].date = new Date(jsonObj[i].date).getTime();
 }
 
+var allTags = getTags();
+var allCats = getCats();
+
+
 var express = require("express"),
 app = express();
 var bodyParser = require('body-parser');
@@ -17,6 +21,14 @@ var port = 8080;
 
 app.get("/", function (req, res) {
     res.redirect("/index.html");
+});
+app.get("/getTags", function (req, res) {
+    res.send(getAllTags());
+	res.end;
+});
+app.get("/getCats", function (req, res) {
+    res.send(getAllCats());
+	res.end;
 });
 
 app.get("/getSortedJSON", function (req, res) {
@@ -70,24 +82,21 @@ function insertionSort(key)
   return arr;
 }
 
-function bubbleSort(key)
-{
+function bubbleSort(key) {
 	var arr = jsonObj;
-	{
-	    for (var i = arr.length - 1; i >= 0; i--)
-	    {
-	        // bubble up
-	        for (var j = 0; j <= i - 1; j++)
-	        {
-	            if (arr[j][key] > arr[j + 1][key])
-					var temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-	        }
-	    }
+	for (var i = arr.length - 1; i >= 0; i--) {
+		// bubble up
+		for (var j = 0; j <= i - 1; j++) {
+	    if (arr[j][key] > arr[j + 1][key]) {
+			var temp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = temp;
+			}
+		}
 	}
-  return arr;
+	return arr;
 }
+
 
 function selectionSort(key)
 {
@@ -109,9 +118,56 @@ function selectionSort(key)
     }
   return arr;
 }
+function getTags() {
+	var tags = [];
+	objs = jsonObj;
+  	for(var i=0; i < objs.length; i++){
+		for (var j = 0; j < objs[i].tags.length; j++) {
+			var tag = objs[i].tags[j].slug;
+			var isPresent = false;
+			for (var k = 0; k < tags.length; k++) {
+				if (tags[k]==tag) {
+					isPresent = true;				}
+			}
+			if (!isPresent) {
+				tags.push(tag);
+			}
+		}
+	
+	}
 
+	return tags;
+}
+function getCats() {
+	var cats = [];
+	objs = jsonObj;
+  	for(var i=0; i < objs.length; i++){
+		for (var j = 0; j < objs[i].categories.length; j++) {
+			var cat = objs[i].categories[j].slug;
+			var isPresent = false;
+			for (var k = 0; k < cats.length; k++) {
+				if (cats[k]==cat) {
+					isPresent = true;
+				}
+			}
+			if (!isPresent) {
+				cats.push(cat);
+			}
+		}
+	
+	}
+	console.log(cats);
+	return cats;
+}
 
-console.log(selectionSort("id"));
+function getAllTags() {
+	return allTags.toString();
+}
+function getAllCats() {
+	return allCats.toString();
+}
+//console.log(getAllTags());
+//console.log(selectionSort("id"));
 //console.log(insertionSort("date"));
 //console.log(bubbleSort("title"));
 //console.log(bubbleSort("id"));
